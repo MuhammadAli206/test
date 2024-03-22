6a46b26455bd2a0c8a72a370b96ed27a5f06f543
MuhammadAli206 <154820260+MuhammadAli206@users.noreply.github.com>
2024-03-22 07:41:46 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+//chapter13
+
+//answer1
+
+let names = []
+names.push("waseem", "Ali","Saeed","Taif","Saif")
+
+console.log(names);
+
+
+//answer2
+
+names.pop()
+console.log(names);
+
+//answer3
+
+names.shift()
+console.log(names);
+
+//answer4
+names.unshift("rajab")
+console.log(names);
+
+//answer6
+
+let nums = ["Gawader","Bolan","Makran","Khuzdar","Panjgor"]
+console.log(nums);
+
+//answer7
+console.log("length...>",nums.length);
+
+//answer8
+let num = ["Gawader","Bolan","Makran","Khuzdar","Panjgor"]
+num.push()
+console.log(num);
+
+
+
+//book questions
+
+
+///answer 1
+
+let ali =[]
+ali.push("karachi","hyderabad","lahore","sahiwal") 
+console.log(ali);
+
+
+///answer 2
+
+let raj =[]
+raj.push(1,2,3,4,5,6)
+console.log(raj);
+
+//answer 3
+
+let array =["Rekhani","Baloch"]
+console.log(array);
+
+//answer 4
+
+let wasee =[12,13,14,15]
+console.log(wasee);
+
+
+//answer 5
+
+let saify =[true,false]
+console.log(saify);
+
+//asnwer 6
+
+let addu =["waseem bhai",16,false]
+console.log(addu);
+
+
+//answer 7
+
+let saidu =["SSC","HSC","BSC","BS","BCOM","MS","M.PHIL","PHD" ]
+console.log(saidu);
+
+///answer 8
+
+
+let maths =[+ prompt('your maths number')];
+let urdu = [+prompt('your urdu number')];
+let english = [+prompt('your english number')];
+
+let percentage =[maths + urdu + english];
+
+console.log(maths + urdu + english)/500 *100
+
+console.log('your percentage is' + percentage +' % ' );
+
+
+
+
+
+
+
